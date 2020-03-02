d413d5171cb103b3f33402431ad773880bdd1395
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-03-02 18:19:26 +0800

    added in users.views on edit page. still working on it. along with the editprofile.html

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,7 +1,8 @@
 import re
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, url_for, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from models.user import User
+from flask_login import login_required, current_user
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -18,16 +19,48 @@ def show(username):
     pass
 
 
-@users_blueprint.route('/', methods=["GET"])
+@users_blueprint.route('/<id>/edit', methods=["GET"])
+@login_required
 def index():
-    return "USERS"
+    users = User.select()
+    return render_template('users/index.html', users=users)
 
 
 @users_blueprint.route('/<id>/edit', methods=['GET'])
+@login_required
 def edit(id):
-    pass
+    if not str(current_user.id) == id:  # this is the current user
+        flash('you are not authorised to view this page fool', 'danger')
+        return redirect(url_for('users.index'))
+
+    user = User.get_or_none(User.id == id)
+
+    if not user:
+        flash('No user found with hte id provided', 'warning')
+        return redirect(url_for('users.index'))
+
+    return render_template('users/edit.html', user=user)
 
 
 @users_blueprint.route('/<id>', methods=['POST'])
+@login_required
 def update(id):
-    pass
+    user = User.get_or_none(User.id == id)
+
+    if not user:
+        flash('no user found with id provided', 'warning')
+        return redirect(url_for('users.index'))
+
+    email = request.form.get('email')
+    username = request.form.get('username')
+
+    user.email = email
+    user.username = username
+
+    if not user.save():
+        for error in user.errors:
+            flash(error, 'warning')
+        return redirect(url_for('users.edit', id=user.id))
+
+    flash('Successfully updated your details', 'success')
+    return redirect(url_for('users.edit', id=user.id))
--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 import peewee as pw
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
+from flask_login import UserMixin
 
 
-class User(BaseModel):
+class User(BaseModel, UserMixin):
     username = pw.CharField(unique=True, null=False)
     email = pw.CharField(unique=True, null=False)
     password = pw.CharField(unique=False, null=False)
@@ -12,6 +14,6 @@ class User(BaseModel):
         duplicate_username = User.get_or_none(User.username == self.username)
         if duplicate_username and not duplicate_username.id == self.id:
             self.errors.append('Username not unique, you siham!')
-        duplicate__email = User.get_or_none(User.email == self.email)
+        duplicate_email = User.get_or_none(User.email == self.email)
         if duplicate_email and not duplicate_email.id == self.id:
             self.errors.append('Email not unique la MCH')
